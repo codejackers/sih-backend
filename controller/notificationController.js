607d20dde5d5e0dfc6fad6b93e3bd78d675c3034
abhishekmorla <abhishekmorla87@gmail.com>
2022-08-25 14:55:05 +0530

    notification model , schema added

---
+++ controller/notificationController.js
@@ -0,0 +1,9 @@
+const QueryInfo = require("../models/NotificationInfo");
+
+const createNotification = async (req, res) => {};
+const deleteNotification = async (req, res) => {};
+
+module.exports = {
+  createNotification,
+  deleteNotification,
+};
